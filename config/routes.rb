680110043ab271b17cd2980707d9fa445bcf8266
rajanand02 <rajanand0202@gmail.com>
2014-05-02 23:27:32 +0530

    add bootstrap and basic layout for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'blog_list#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
